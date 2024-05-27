8594e4bc1e33881320d3ae0d2fb7960199a15fff
Brandon <bray4168@yahoo.com>
2024-05-27 14:54:26 -0500

    Add basic user input to start setting up the game state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
     println!("Let's make love letter!");
+
+    let mut user_input: String = String::new();
+    println!("Input player name...");
+    io::stdin().read_line(&mut user_input).unwrap();
+    println!("User input: {}", user_input);    
 }
