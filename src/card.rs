c5abcae24d7ac945e7c587a5adada71056a61824
Brandon <bray4168@yahoo.com>
2024-05-27 15:31:56 -0500

    Start creating cards for a deck

---
+++ src/card.rs
@@ -0,0 +1,8 @@
+#[derive(Debug)]
+pub struct Card {
+    pub name: String,
+    pub description: String,
+    pub value: u32,
+    pub is_face_down: bool,
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+mod card;
+
 use std::io;
+use crate::card::Card;
 
 fn main() {
     println!("Let's make love letter!");
@@ -7,4 +10,12 @@ fn main() {
     println!("Input player name...");
     io::stdin().read_line(&mut user_input).unwrap();
     println!("User input: {}", user_input);    
+
+    let card: Card = Card {
+        name: String::from("Princess"),
+        description: String::from("If you discard this card, you are out of the round."),
+        value: 8,
+        is_face_down: false,
+    };
+    println!("{:?}", card);
 }
